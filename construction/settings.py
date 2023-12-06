b2294b7dc48d13c799684911e80f756ea43f6853
jackjakarta <al.termure@gmail.com>
2023-12-06 05:07:20 +0100

    settings update

--- construction/settings.py
+++ construction/settings.py
@@ -151,7 +151,7 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'unst wevt gkuj wrbh'
 
 
 django_heroku.settings(locals())
