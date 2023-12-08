7ea59427be18a59be9b5a5ba120ef25b0cf52802
jackjakarta <al.termure@gmail.com>
2023-12-08 14:51:04 +0100

    model added to admin

--- website/admin.py
+++ website/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import NewsletterSub, FinishedBuilding, Employee
+from .models import NewsletterSub, FinishedBuilding, Employee, PartnerCompany
 
 # Register your models here.
 admin.site.register(NewsletterSub)
 admin.site.register(FinishedBuilding)
 admin.site.register(Employee)
+admin.site.register(PartnerCompany)
